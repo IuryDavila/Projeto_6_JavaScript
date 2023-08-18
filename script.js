e42a9e9bb290ca82c17442d6a055befd4702c954
IuryDavila <sc_contato@outlook.com>
2023-08-18 00:24:40 -0300

    progresso com js

--- script.js
+++ script.js
@@ -7,7 +7,65 @@ class Despesa{
 		this.descricao = descricao
 		this.valor = valor
 	}
+
+	validacao(){
+		for(let i in this ){
+			if(this[i] == undefined || this[i] == '' || this[i] == null){
+				return false
+			}
+		}
+		return true
+	}
 }
+
+class BancoDeDados{
+
+	constructor(){
+		let id = localStorage.getItem('id')
+			if(id === null){
+				localStorage.setItem('id', 0)
+
+			}
+
+	}
+
+	getproximoID(){	
+	let pegarproximoID = localStorage.getItem('id')
+	return (parseInt(pegarproximoID) + 1)
+	}
+
+
+	 	gravar(d){
+		let id = this.getproximoID()
+		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem('id', id)
+
+
+	}
+
+	registros(){
+		let despesas = Array()
+
+		let id = localStorage.getItem('id')
+		for(let i = 1; i <= id; i++){
+			let despesa = JSON.parse(localStorage.getItem(i))
+
+			if(despesa === null){
+				continue
+			}
+
+			despesas.push(despesa)
+		}
+
+		return despesas
+	}
+
+
+}
+
+let bancodedados = new BancoDeDados()
+	
+
 
 function adicionar() {
 	let ano = document.getElementById('ano')
@@ -25,18 +83,26 @@ function adicionar() {
 			dia.value,
 			tipo.value,
 			descricao.value,
-			valor.value,
+			valor.value
 		)
 
-			gravar(despesa)
+	if(despesa.validacao()){
+		bancodedados.gravar(despesa)
+		alert('dados gravados com sucesso')
+
+		
+	} else{
+		alert('faltam informações')
 	}
 
-	function gravar(d){
-		localStorage.setItem('despesa', JSON.stringify(d))
+}
 
-	}
+function CarregarDados(){ 
+	let despesas = Array()
+	despesas = bancodedados.registros()
 
-
+	console.log(despesas)
+}
 
 
 	
